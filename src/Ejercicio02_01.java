430d39a9b536c05b7605f7e127de6df77d6c82d4
is13mael <ismaelparramoussaif@gmail.com>
2025-09-26 22:33:51 +0200

    Ejercicio02_01.java

---
+++ src/Ejercicio02_01.java
@@ -0,0 +1,14 @@
+public class Ejercicio02_01 {
+    public static void main(String[] args){
+    int x;
+    int y;
+    x = 144;
+    y = 999;
+    System.out.println("x = " + x);
+    System.out.println("y = " + y);
+    System.out.println("x + y = " + (x + y));
+    System.out.println("x - y = " + (x - y));
+    System.out.println("x / y = " + ((float) x / (float) y));
+    System.out.println("x * y = " + (x * y)); 
+    }
+}
