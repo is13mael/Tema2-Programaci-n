c3eb2c09ceb0abd3529f9cf087ee019a32c62c2c
is13mael <ismaelparramoussaif@gmail.com>
2025-09-26 21:32:36 +0200

    Added Ejemplo02_01_02

---
+++ src/Ejemplo02_01_02.java
@@ -0,0 +1,15 @@
+/**
+ * Uso de variables que contienen números decimales
+ */
+public class Ejemplo02_01_02 {
+    public static void main(String[] args){
+        double x; //Se declaran las variables x e y
+        double y; //de tal forma que pueden almacenar decimales.
+
+        x = 7;
+        y = 25.01;
+
+        System.out.println(" x vale " + x);
+        System.out.println(" y vale " + y);
+    }
+}
