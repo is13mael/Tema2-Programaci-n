faf6d136753fb2d778f2a47c34e1a578ad4b2d6e
is13mael <ismaelparramoussaif@gmail.com>
2025-09-26 21:54:32 +0200

    CharcomoInt.java

---
+++ src/CharcomoInt.java
@@ -0,0 +1,14 @@
+/**
+ * Correspondencia entre los tipos char e int
+ */
+public class CharcomoInt {
+    public static void main(String[] args){
+        char letra1 = 'a';
+        char letra2 = 'b';
+
+        System.out.println(letra1);
+        System.out.println(letra2);
+        System.out.println(letra1 + letra2);
+        System.out.println(letra1+ "" + letra2);
+    }
+}
