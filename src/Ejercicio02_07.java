f4a4e31cf4d55dae33927b00516b3d8c05a7007f
is13mael <ismaelparramoussaif@gmail.com>
2025-09-26 23:22:50 +0200

    Ejercicio02_07.java

---
+++ src/Ejercicio02_07.java
@@ -0,0 +1,9 @@
+public class Ejercicio02_07 {
+    public static void main(String[] args){
+        char PrimeraLetra = 'a';
+        char UltimaLetra = 'z';
+        String Palabra = "abecedario";
+
+        System.out.println(PrimeraLetra + "" + UltimaLetra + Palabra);
+    }
+}
