29e4e708f3cf0d7483ff18d1f38ed6be89b1c183
is13mael <ismaelparramoussaif@gmail.com>
2025-09-26 22:06:46 +0200

    UsoDeOperadoresAritmeticos.java

---
+++ src/UsoDeOperadoresAritmeticos.java
@@ -0,0 +1,11 @@
+/**
+ * Uso de Operadores Aritméticos
+ */
+public class UsoDeOperadoresAritmeticos {
+    public static void main(String[] args){
+        int x;
+        x = 100;
+        System.out.println(x + " " + (x + 5) + " " + (x - 5));
+        System.out.println((x * 5) + " " + (x / 5) + " " + (x % 5));
+    }
+}
