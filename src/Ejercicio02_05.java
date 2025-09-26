2b0afa4409da01b4f0fe528d99e76e77da52cdcc
is13mael <ismaelparramoussaif@gmail.com>
2025-09-26 23:01:50 +0200

    Ejercicio02_05.java

---
+++ src/Ejercicio02_05.java
@@ -0,0 +1,9 @@
+public class Ejercicio02_05 {
+    public static void main(String[] args){
+        int pesetas = 10000;
+        double euros = pesetas / 166.386;
+
+        
+        System.out.printf("%d pesetas %.2f euros.%n", pesetas, euros);
+    }
+}
