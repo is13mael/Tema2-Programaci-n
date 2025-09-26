a9449bf47176b5661425cb983165171acc48cf1f
is13mael <ismaelparramoussaif@gmail.com>
2025-09-26 22:47:35 +0200

    Ejercicio02_03.java

---
+++ src/Ejercicio02_03.java
@@ -0,0 +1,11 @@
+public class Ejercicio02_03 {
+    public static void main(String[] args){
+        String miNombre = "Ismael Parra Moussaif";
+        String miDireccion = "Calle Rodahuevos, 69 - Campanillas (Málaga) - España";
+        String miTelefono = "644273438";
+
+        System.out.println(miNombre);
+        System.out.println(miDireccion);
+        System.out.println(miTelefono);
+    }
+}
